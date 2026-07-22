d41e0a9776c2c394d36202c86ac102af03e3a401
Mega-Sim <sik1093@naver.com>
2026-07-21 21:56:55 -0400

    불필요한 빌드 산출물 제거 / Remove generated build artifacts

--- build/CMakeFiles/sim_core_cli.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for sim_core_cli.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/Desktop/Sim_Core
-# Build directory: /home/user/Desktop/Sim_Core/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test("sim_core_tests" "/home/user/Desktop/Sim_Core/build/sim_core_tests")
-set_tests_properties("sim_core_tests" PROPERTIES  _BACKTRACE_TRIPLES "/home/user/Desktop/Sim_Core/CMakeLists.txt;58;add_test;/home/user/Desktop/Sim_Core/CMakeLists.txt;0;")
-subdirs("_deps/nlohmann_json-build")
